df9cabec9a9e2f9108f6f81f5871fe1473206924
kajal021 <kajal@eazotel.com>
2026-09-04 16:47:55 +0530

    Add target and rel attributes to Link component

--- src/components/buttons/LinkButton.tsx
+++ src/components/buttons/LinkButton.tsx
@@ -69,6 +69,8 @@ const LinkButton: React.FC<LinkButtonProps> = ({
         </button>
       ) : (
         <Link
+          target="_blank"
+        rel="noreferrer"
           href={href}
           className={`flex items-center tracking-widest gap-1 border w-fit px-4 py-2 hover:scale-95 transition-all duration-300 ease-in-out hover:scale-x-105 active:scale-95 ${className}`}
           {...props}
